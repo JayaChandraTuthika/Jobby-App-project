43081fb488d3bb1aecd1f903e03ed308c2a97c6e
JayaChandraTuthika <chandra4204@gmail.com>
2023-02-17 14:35:45 +0000

    pending

---
+++ src/components/LoginPage/index.js
@@ -0,0 +1,17 @@
+import {Component} from 'react'
+
+import './index.css'
+
+class LoginPage extends Component {
+  render() {
+    return (
+      <div className="login-bg">
+        <form className="login-form">
+          <h1>Hello</h1>
+        </form>
+      </div>
+    )
+  }
+}
+
+export default LoginPage
